a31361df3d81f950e239f537fb2ba4b4383b93c8
Inori-dokusha <inori.dokusha@gmail.com>
2025-07-06 17:50:39 +0700

    update

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // Membuat tombol piihan Batu, Gunting dan Kertas
 function buatTombol(element, arrayTeks) {
+  const wrap = document.createElement("div");
   for (let i = 0; i < element; i++) {
-    let button = document.createElement("button");
-    let teks = document.createTextNode(arrayTeks[i]);
+    const button = document.createElement("button");
+    const teks = document.createTextNode(arrayTeks[i]);
     button.appendChild(teks);
-    document.body.appendChild(button, teks);
+    wrap.appendChild(button, teks);
+    document.body.appendChild(wrap, button, teks);
     button.style.cursor = "pointer";
   }
+  wrap.setAttribute("id", "wrap");
 }
 buatTombol(3, ["Batu", "Gunting", "Kertas"]);
 
@@ -26,52 +29,73 @@ function komputer(komp) {
 
 // Aturan main
 function aturanMain() {
+  const container = document.createElement("div");
+  const info = document.createElement("h2");
+  container.setAttribute("id", "popup");
+  container.appendChild(info);
+  
+  document.body.appendChild(container, info);
+  
   // mengambil button pada document
   const button = document.getElementsByTagName("button");
+  
+  // aturannya
   for (let i = 0; i < button.length; i++) {
     const element = button[i];
     element.addEventListener("click", () => {
       if (element.textContent === "Batu" && komputer() === "Gunting") {
-        console.log("Kamu Menang ,Komputer Kalah");
+        info.textContent = "Kamu Menang ,Komputer Kalah";
       } else if (element.textContent === "Batu" && komputer() === "Kertas") {
-        console.log("Kamu Kalah, Komputer Menang");
+        info.textContent = "Kamu Kalah, Komputer Menang";
       } else if (element.textContent === "Gunting" && komputer() === "Batu") {
-        console.log("Kamu Kalah, Komputer Menang");
+        info.textContent = "Kamu Kalah, Komputer Menang";
       } else if (element.textContent === "Gunting" && komputer() === "Kertas") {
-        console.log("Kamu Menang, Komputer Kalah");
+        info.textContent = "Kamu Menang, Komputer Kalah";
       } else if (element.textContent === "Kertas" && komputer() === "Batu") {
-        console.log("Kamu Menang, Komputer Kalah");
+        info.textContent = "Kamu Menang, Komputer Kalah";
       } else {
-        console.log("SERI");
+        info.textContent = "SERI";
       }
     });
   }
 }
-aturanMain();
+
+// Menambahkan Skor
+function tambahSkor(judulKontainer) {
+  // Buat elemen div sebagai pembungkusnya
+  const div1 = document.createElement("div");
+  judulKontainer = document.createElement(`${judulKontainer}`);
+  div1.setAttribute("id", "container");
+  
+  // informasi skor
+  const div2 = document.createElement("div");
+  div2.setAttribute("class", "badan-skor")
+  
+  div1.appendChild(judulKontainer, div2);
+  judulKontainer.setAttribute("class", "judul-container");
+  judulKontainer.textContent = "Hasil Permainan";
+  
+  document.body.appendChild(div1, judulKontainer, div2);
+  
+  // fungsi untuk menambahkan skor otomatis
+}
+tambahSkor("h2");
 
 // Ubah warna pada button secara acak
 function ubahWarna() {
   const button = document.querySelectorAll("button");
-
+  
   button.forEach((element) => {
     setInterval(() => {
       const r = Math.floor(Math.random() * 1 * 255);
       const g = Math.floor(Math.random() * 1 * 255);
       const b = Math.floor(Math.random() * 1 * 255);
-
+      
       element.addEventListener("click", () => {
         element.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-
-        if (r <= 30 && g <= 30 && b <= 30) {
-          element.style.color = "white";
-          console.log(true);
-        } else {
-          element.style.color = "black";
-          console.log(false);
-        }
       });
     }, 500);
   });
+  aturanMain();
 }
-
 ubahWarna();
